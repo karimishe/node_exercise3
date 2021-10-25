c10ce24191efae015e2bb6bc5b13a3ae7f035bd0
karimishe <ildarkaimov22@gmail.com>
2021-10-26 01:07:04 +0500

    adding pug templating engines

--- app.js
+++ app.js
@@ -4,23 +4,25 @@ const path = require('path')
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const rootDir = require('./util/path');
-
 
 const { application } = require('express');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views')
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+    res.status(404).render('404', {pageTitle: "Page not found"});
 })
 
 app.listen(3003)
--- routes/admin.js
+++ routes/admin.js
@@ -6,16 +6,18 @@ const bodyParser = require('body-parser')
 
 const router = express.Router();
 
-router.get('/add-product', (req, res, next) => {
+const products = [];
 
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+router.get('/add-product', (req, res, next) => {
+    res.render('add-product', {pageTitle: "Add Product", path: '/admin/add-product'})
 });
 
 router.post('/add-product', (req, res, next) => {
-    console.log(req.body);
+    products.push({title: req.body.title})
     res.redirect('/');
 })
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
 
 
